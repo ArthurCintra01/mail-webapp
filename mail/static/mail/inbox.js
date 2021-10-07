bf0a6a50da3624e4490ec453c97f57e0a2b50e56
ArthurCintra01 <turcko.cintra@gmail.com>
2021-10-06 21:21:53 -0300

    archive/unarchive

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -41,17 +41,62 @@ function load_mailbox(mailbox) {
       timestamp = emails[email].timestamp;
       let div = document.createElement('div');
       if (emails[email].read === true){
-        div.style = 'cursor: pointer; background-color: #c0c0c0; border: 1px solid black; margin-top: 10px; padding: 10px;';
+        div.style = 'display: inline-block; width: 91%; cursor: pointer; background-color: #c0c0c0; border: 1px solid black; margin-top: 10px; padding-top: 10px;  padding-bottom: 10px;  padding-left: 20px;';
       }else{
-        div.style = 'cursor: pointer; border: 1px solid black; margin-top: 10px; padding: 10px;';
+        div.style = 'display: inline-block; width: 91%; cursor: pointer; border: 1px solid black; margin-top: 10px; padding-top: 10px;  padding-bottom: 10px;  padding-left: 20px;';
       }
       div.innerHTML = `<div style="display:inline-block;"><strong>${sender}</strong></div>
-      <div style="display:inline-block; padding-left: 15px; width: 70%;">${subject}</div>
+      <div style="display:inline-block; padding-left: 15px; width: 60%;">${subject}</div>
       <div style="display:inline-block;">${timestamp}</div>`;
+      let btn = document.createElement('button');
+      if (emails[email].archived === true){
+        btn.innerHTML = 'Unarchive';
+        btn.style = 'font-size: 1.2em; margin-left: 10px; padding:8px;';
+        if (emails[email].read === true){
+          div.style = 'background-color: #c0c0c0; display: inline-block; width: 89%; cursor: pointer; border: 1px solid black; margin-top: 10px; padding-top: 10px;  padding-bottom: 10px;  padding-left: 20px;';
+        }else{
+          div.style = 'display: inline-block; width: 89%; cursor: pointer; border: 1px solid black; margin-top: 10px; padding-top: 10px;  padding-bottom: 10px;  padding-left: 20px;';
+        }
+      }else{
+        btn.innerHTML = 'Archive'; 
+        btn.style = 'font-size: 1.2em; margin-left: 10px; padding:8px;';
+      }
+      btn.addEventListener('click', () => archive(emails[email]));
+      btn.classList = 'btn btn-sm btn-outline-primary';
       div.addEventListener('click', () => load_email(emails[email]));
+      if(mailbox === 'sent'){
+        div.style.width = '100%';
+      }
       document.querySelector('#emails-view').append(div);
+      if (mailbox != 'sent'){
+        document.querySelector('#emails-view').append(btn);
+      }
     }
   })
+}
+
+function archive(email){
+  if(email.archived === true){
+    fetch(`/emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: false
+      })
+    })
+    .then(() => {
+        load_mailbox('archive');
+    });
+  }else{
+    fetch(`/emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: true
+      })
+    })
+    .then(() => {
+        load_mailbox('inbox');
+    });
+  }
 }
 
 function load_email(email){
@@ -66,7 +111,8 @@ function load_email(email){
   <strong>To:</strong> ${email.recipients}<br>
   <strong>Subject:</strong> ${email.subject}<br>
   <strong>Timestamp:</strong> ${email.timestamp}<br>
-  <button id="reply" style="font-size: 0.8em; padding: 6px; margin-top:10px;"class="btn btn-outline-primary">Reply</button></div>
+  <button id="reply" style="font-size: 0.8em; padding: 6px; margin-top:10px;"class="btn btn-outline-primary">Reply</button>
+  <button id="archive" style="font-size: 0.8em; padding: 6px; margin-top:10px;"class="btn btn-outline-primary">Archive</button></div>
   <p style="margin-top:10px;">${email.body}</p>`;
 }
 
@@ -84,10 +130,8 @@ function send_email(event){
     })
   })
   .then(response => response.json())
-  .then(result => {
-      // Print result
-      console.log(result);
+  .then( () => {
+      load_mailbox('sent');
   });
-  load_mailbox('sent');
 } 
 
